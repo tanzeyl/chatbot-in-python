48e8fcd9a88a46b2493d03983374248da71363fe
Tanzeel Khan <ktanzeel80@gmail.com>
2021-06-29 17:33:15 +0530

    Creating logic

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+app = Flask(__name__)
+
+english_bot = ChatBot("Chatterbot", storage_adapter = "chatterbot.storage.SQLStorageAdapter")
+trainer = ChatterBotCorpusTrainer(english_bot)
+trainer.train(chatterbot.corpus.english)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+if __name__ == "__main__":
+    app.run(debug = True)
