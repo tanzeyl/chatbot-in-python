6d7f1186cef749f67a9b878f45c7befb65310c90
Tanzeel Khan <ktanzeel80@gmail.com>
2021-07-01 18:14:09 +0530

    Fixing UI

--- app.py
+++ app.py
@@ -4,7 +4,18 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
 
-english_bot = ChatBot("Chatterbot", storage_adapter = "chatterbot.storage.SQLStorageAdapter")
+english_bot =  ChatBot('Chatterbot',
+             logic_adapters = [
+                 {
+                     'import_path': 'chatterbot.logic.BestMatch',
+                     'default_response': 'I am sorry, I do not understand. I am still learning. Please contact ktanzeel80@gmail.com for further assistance.',
+                     'maximum_similarity_threshold': 0.90
+                 }
+             ],
+             read_only = True,
+             preprocessors=['chatterbot.preprocessors.clean_whitespace',
+'chatterbot.preprocessors.unescape_html',
+'chatterbot.preprocessors.convert_to_ascii'])
 trainer = ChatterBotCorpusTrainer(english_bot)
 trainer.train("chatterbot.corpus.english")
 trainer.train("data/data.yml")
